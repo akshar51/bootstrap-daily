1823251bf9a62e1fda0789b439c1dbd08ac72e2a
akshar66 <aksharparekh401@gmail.com>
2024-07-04 20:33:16 +0530

    compplete

---
+++ L11/script.js
@@ -0,0 +1,13 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+$(document).ready(function(){
+    $(".toast").toast("show");
+})
